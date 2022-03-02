f671ab8553d03bbab0fbb941240bdb729bdc0274
apelland1 <100165860+apelland1@users.noreply.github.com>
2022-03-02 11:50:49 -0700

    Create broken.py

---
+++ broken.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import random
+
+print("Think of a number between 1 and 10")
+answer = ""
+while answer != "y":
+    print("Is " + str(random.randint(1, 10)) + " your number? ('y' for yes, any key for no)")
+    answer = input()
+print("I guessed your number!")
