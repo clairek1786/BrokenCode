1ba5d346b6e57e48a3ab801daca248edc28119d1
clairek1786 <98114387+clairek1786@users.noreply.github.com>
2022-03-06 16:52:01 -0700

    Update broken.py

--- broken.py
+++ broken.py
@@ -5,4 +5,5 @@ answer = ""
 while answer != "y"
     print("Is " + str(random.randint(1, 10)) + " your number? ('y' for yes, any key for no)")
     answer = input()
-Print("I guessed your number!")
+print("I guessed your number!")
+# the p was capitalized
